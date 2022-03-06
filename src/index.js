3bea4c95eeff88af6b25d52a6f84f98d2f92ee71
Noah Reece <noah.w.reece@gmail.com>
2022-03-05 21:40:15 -0500

    added index.js structure text

---
+++ src/index.js
@@ -0,0 +1,46 @@
+//#region GLOBAL VARIABLES
+// ----------------------------------------------------------------------
+// Declare any global variables we need in this region
+// ----------------------------------------------------------------------
+// ▼ (Type below this line) ▼
+
+
+
+//#endregion
+
+
+//#region CODE RUN ON DOC LOAD
+// ----------------------------------------------------------------------
+// Any code that should actually be run upon this file being loaded 
+// (other than global variable declarations) should go in this region.
+// This includes things like function invocations
+// ----------------------------------------------------------------------
+// ▼ (Type below this line) ▼
+
+
+
+//#endregion
+
+
+//#region FUNCTIONS - WORKING
+// ----------------------------------------------------------------------
+// Put any functions here that are complete and part of our working code
+// ----------------------------------------------------------------------
+// ▼ (Type below this line) ▼
+
+
+
+//#endregion
+
+
+//#region FUNCTIONS - IN-PROGRESS
+// ----------------------------------------------------------------------
+// Use the space below to when writing new functions.
+// Once you are satisfied with how the function looks/works, move it to
+// the region above.
+// ----------------------------------------------------------------------
+// ▼ (Type below this line) ▼
+
+
+
+//#endregion
